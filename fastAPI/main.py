67c538597b3ca46f217e5f13558b078630dcdec7
Lixang Li <lli32@myune.edu.au>
2024-12-29 01:42:15 +1100

    Added MyCoffee class to make typesafe outputs from the coffee agent

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -4,19 +4,26 @@ from pydantic import BaseModel
 from pydantic_ai import Agent
 from pydantic_ai.models.gemini import GeminiModel
 
+class MyCoffee(BaseModel): 
+    itemID: int
+    size: str
+    itemName: str
+    modifiers:str
+
 app = FastAPI()
 MY_KEY = os.getenv('GEMINI_API_KEY')
 if not MY_KEY:
     raise ValueError("GEMINI_API_KEY environment variable not found")
 
 model = GeminiModel("gemini-2.0-flash-exp",api_key = MY_KEY )
-coffeeAgent = Agent(model=model)
+coffeeAgent = Agent(model=model,result_type=MyCoffee)
 judgeAgent = Agent(model=model)
 
-result = coffeeAgent.run_sync("Test waht is the capital of Mexico?")
+result = coffeeAgent.run_sync("What is an americano?")
 
-print (result.data)
-
+print(result.data)
+judgeResult = judgeAgent.run_sync("Test waht is the capital of Mexico?")
+print("Judge agent:" + judgeResult.data)
 # @app.post("/api/chat")
 # async def chat_endpoint(message: str):
 #     result = await agent.run(message)
